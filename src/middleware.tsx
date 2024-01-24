4e1407afd27353802a086be56908d123ba30fafe
Valery <valerkaogorodnikov@mail.ru>
2024-01-24 23:00:14 +0300

    add ref tok

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,52 +1,55 @@
+import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
 
+import { API_SERVER_URL } from './configs/api.config'
 import { EnumTokens } from './services/auth/auth.service'
+import { IUser } from './services/user/user.interface'
 
 //запрос на стороне сервера
-// const fetchGetProfile = async () => {
-// 	'use server'
+const fetchGetProfile = async () => {
+	'use server'
 
-// 	const cookie = cookies()
-// 	const accessToken = cookie.get(EnumTokens.ACCESS_TOKEN)?.value
+	const cookie = cookies()
+	const accessToken = cookie.get(EnumTokens.ACCESS_TOKEN)?.value
 
-// 	return fetch(`${API_SERVER_URL}/users/profile`, {
-// 		method: 'get',
-// 		headers: {
-// 			Authorization: `Bearer ${accessToken}`,
-// 		},
-// 		credentials: 'include',
-// 	}).then(res => res.json()) as Promise<IUser>
-// }
+	return fetch(`${API_SERVER_URL}/users/profile`, {
+		method: 'get',
+		headers: {
+			Authorization: `Bearer ${accessToken}`,
+		},
+		credentials: 'include',
+	}).then(res => res.json()) as Promise<IUser>
+}
 
 //проверка на стороне сервера
 export async function middleware(request: NextRequest, response: NextResponse) {
-	const refreshToken =
-		request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value || ''
-	console.log(refreshToken)
 	const path = request.nextUrl.pathname
 	const isPublicPath = path === '/login' || path === '/register'
 	const isAdminPath = request.url.includes('/admin')
+
+	try {
+		const UserData = await fetchGetProfile()
+
+		// if (UserData?.isAdmin === true) return NextResponse.next()
+
+		// if (isAdminPath) {
+		// 	return NextResponse.rewrite(new URL('/404', request.url))
+		// }
+
+		// return NextResponse.next()
+	} catch (error) {
+		// request.cookies.delete(EnumTokens.ACCESS_TOKEN)
+		// return redirectToLogin(isAdminPath, isPublicPath, request)
+	}
+
+	const refreshToken =
+		request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value || ''
 
 	if (!isPublicPath && !refreshToken) {
 		return NextResponse.redirect(new URL('/login', request.nextUrl))
 	}
 	// else if (isPublicPath && refreshToken) {
 	// 	return NextResponse.redirect(new URL('/', request.nextUrl))
-	// }
-
-	// try {
-	// 	// const UserData = await fetchGetProfile()
-
-	// 	// if (UserData?.isAdmin === true) return NextResponse.next()
-
-	// 	if (isAdminPath) {
-	// 		return NextResponse.rewrite(new URL('/404', request.url))
-	// 	}
-
-	// 	return NextResponse.next()
-	// } catch (error) {
-	// 	request.cookies.delete(EnumTokens.ACCESS_TOKEN)
-	// 	return redirectToLogin(isAdminPath, isPublicPath, request)
 	// }
 
 	return NextResponse.next()
